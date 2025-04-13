2e7fa37ee9606acdb97cc9ba465d8dfbe3d21779
narulsa1 <sahil.narula04@gmail.com>
2025-04-13 15:42:14 +0530

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -98,12 +98,12 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.35.0"
+  version = "20.24.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.31"
+  cluster_version = "1.30"
 
-  enable_cluster_creator_admin_permissions = true
+  cluster_endpoint_public_access  = true
 
   cluster_addons = {
     coredns                = {}
@@ -111,11 +111,11 @@ module "eks" {
     kube-proxy             = {}
     vpc-cni                = {}
   }
- 
+
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
-  
+
   eks_managed_node_groups = {
     karpenter = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
@@ -125,8 +125,10 @@ module "eks" {
       min_size     = 2
       max_size     = 10
       desired_size = 2
-
+      
       taints = {
+        # This Taint aims to keep just EKS Addons and Karpenter running on this MNG
+        # The pods that do not tolerate this taint should run on nodes created by Karpenter
         addons = {
           key    = "CriticalAddonsOnly"
           value  = "true"
@@ -136,8 +138,21 @@ module "eks" {
     }
   }
 
+  # Cluster access entry
+  # To add the current caller identity as an administrator
+  enable_cluster_creator_admin_permissions = true
+
+  node_security_group_tags = {
+    # NOTE - if creating multiple security groups with this module, only tag the
+    # security group that Karpenter should utilize with the following tag
+    # (i.e. - at most, only one security group should have this tag in your account)
+    "karpenter.sh/discovery" = var.cluster_name
+  }
 }
 
+###########################################################################################
+# Karpenter
+###########################################################################################
 module "karpenter" {
   source = "terraform-aws-modules/eks/aws//modules/karpenter"
 
@@ -146,15 +161,14 @@ module "karpenter" {
   enable_v1_permissions = true
 
   enable_pod_identity             = true
-  enable_pod_identity_association = true
+  create_pod_identity_association = true
 
   # Attach additional IAM policies to the Karpenter node IAM role
   node_iam_role_additional_policies = {
     AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   }
-
-
 }
+
 
 ############################################################################################
 # Karpenter Helm
