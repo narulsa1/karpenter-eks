cb7690b32677abb3b73009f5ac9f9a9c975fadd8
narulsa1 <sahil.narula04@gmail.com>
2025-04-13 14:58:30 +0530

    added EKS code

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,3 +49,50 @@ module "vpc" {
   }
 
 }
+
+
+############################################################################################
+# EKS
+############################################################################################
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.35.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.31"
+
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
+  }
+ 
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
+  
+  eks_managed_node_groups = {
+    karpenter = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["m5.large"]
+
+      min_size     = 2
+      max_size     = 10
+      desired_size = 2
+
+      taints = {
+        addons = {
+          key    = "CriticalAddonsOnly"
+          value  = "true"
+          effect = "NO_SCHEDULE"
+        },
+      }
+    }
+  }
+
+}
