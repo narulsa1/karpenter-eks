cb416dfd4f7fd397229b583fbcafef59347fedd9
narulsa1 <sahil.narula04@gmail.com>
2025-04-13 15:09:54 +0530

    added karpenter code

--- terraform/main.tf
+++ terraform/main.tf
@@ -96,3 +96,21 @@ module "eks" {
   }
 
 }
+
+module "karpenter" {
+  source = "terraform-aws-modules/eks/aws//modules/karpenter"
+
+  cluster_name = module.eks.cluster_name
+
+  enable_v1_permissions = true
+
+  enable_pod_identity             = true
+  enable_pod_identity_association = true
+
+  # Attach additional IAM policies to the Karpenter node IAM role
+  node_iam_role_additional_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+
+
+}
